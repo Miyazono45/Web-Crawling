388a43ad699ed3cd208f477383e98b25cfdad2eb
Miyazono45 <rifqiromadhoni44@gmail.com>
2023-11-27 16:22:28 +0700

    Testing

--- webcrawl.py
+++ webcrawl.py
@@ -6,17 +6,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-url = "https://www.tokopedia.com/nanokomputer/product?perpage=20"
-driver = webdriver.Chrome()
-driver.get(url)
+# User Input
+harga = input("Harga VGA nya (Angka saja -> satuan juta) = ")
+# bekasOrBaru = input("Preferensi Bekas / Baru = ")
 
-data = []
-soup = BeautifulSoup(driver.page_source, "html.parser")
-container = soup.findAll('div', attrs={'class': 'css-1asz3by'})
-for divContainer in container:
-    nama_barang = divContainer.find(
-        'div', attrs={'class': 'prd_link-product-name'}).text
-    data.append((nama_barang))
+# Set url dengan harga asli dan harga 1 juta dibawahnya
+url = "https://www.tokopedia.com/search?st=&q=VGA%20" + harga + "%20Juta"
+urlMinus = "https://www.tokopedia.com/search?st=&q=VGA%20" + \
+    str(int(harga) - 1) + "%20Juta"
 
-df = pd.DataFrame(data, columns=['Nama Barang'])
-print(df)
+# Penyimpanan Data
+data1 = []
+data2 = []
+
+
+def crawlData(urlWhat):
+    driver = webdriver.Chrome()
+    driver.get(url if urlWhat == 0 else urlMinus)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    container = soup.findAll('div', attrs={'class': 'css-1asz3by'})
+    for divContainer in container:
+        nama_barang = divContainer.find(
+            'div', attrs={'class': 'prd_link-product-name'}).text
+        (data1 if urlWhat == 0 else data2).append((nama_barang))
+
+
+for i in range(2):
+    crawlData(i)
+
+dataRTX = [item for item in data1 if "RTX" in item]
+dataRX = [item for item in data1 if "RX" in item]
+
+dfRTX = pd.DataFrame(dataRTX, columns=['RTX'])
+dfRX = pd.DataFrame(dataRX, columns=['RX'])
+
+print(dfRTX)
+print("")
+print(dfRX)
