23c10d28dfa2daea61664d6d39277b5e9da0f80c
YunusDH <112492147+Teheft1@users.noreply.github.com>
2023-12-03 21:24:11 +0700

    Adding a little kinda DFS

--- Crawl.py
+++ Crawl.py
@@ -1,74 +1,98 @@
-from bs4 import BeautifulSoup
 import time
+import re
+import numpy as np
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
+
 
 
 #User Input
-quer = input('whats your Searching = ')
-pricemax = input('what is max price = ')
-pricemin = input('what is min price = ')
+# quer = input('whats your Searching = ')
+# pricemin = input('what is min price = ')
+# pricemax = input('what is max price = ')
 
 
 urls = 'https://www.tokopedia.com'
-# response = requests.get(urls)
-# print(response)
-# option =  Options()
-# option.add_argument('--headless')\
+
 
 
 driver = webdriver.Chrome()
 driver.get(urls)
-# Input User Search Query
 driver.find_element(By.TAG_NAME,'input')
 data_input = driver.find_element(By.CLASS_NAME,'css-3017qm')
 ActionChains(driver)\
-    .send_keys_to_element(data_input,quer)\
+    .send_keys_to_element(data_input,'VGA Graphic Card')\
     .key_down(Keys.RETURN)\
     .perform()
-time.sleep(4)
+time.sleep(1.5)
 driver.current_url
 
-# Shift it to highest or lowest(not yet done)
 driver.find_element(By.CLASS_NAME,'css-1g467vj').click()
 driver.find_elements(By.CLASS_NAME, 'e83okfj5')
-driver.find_element(By.CSS_SELECTOR,'[data-item-text="Harga Tertinggi"]').click()
-time.sleep(3)
+driver.find_element(By.CSS_SELECTOR,'[data-item-text="Harga Terendah"]').click()
+time.sleep(1)
 
 driver.current_url
 
-# Setting the value minimum
 driver.find_element(By.CLASS_NAME, 'css-1cb34wj')
 min = driver.find_element(By.NAME,'pmin')
 ActionChains(driver)\
     .send_keys_to_element(min,'0')\
-    .send_keys_to_element(min,pricemin)\
+    .send_keys_to_element(min,'10000000')\
     .key_down(Keys.RETURN)\
     .perform()
-time.sleep(3)
-
-# setting the value maximum
+time.sleep(1)
 driver.find_element(By.CLASS_NAME, 'css-1cb34wj')
 max = driver.find_element(By.NAME,'pmax')
 ActionChains(driver)\
     .send_keys_to_element(max,'0')\
-    .send_keys_to_element(max,pricemax)\
+    .send_keys_to_element(max,'15000000')\
     .key_down(Keys.RETURN)\
     .perform()
-time.sleep(3)
-driver.current_url
-print(driver.current_url)
+time.sleep(1)
+primaryUrl = driver.current_url
+
+page = 1
+
+visited = []
+def web_crawl(Url,page,mainUrl):
+    # driver = webdriver.Chrome()
+    print(f"crawling data in : {page} pages")
+    time.sleep(1)
+
+    # Just A little bit more (I am Lazy)
+    if Url in visited:
+          return
+    visited.append(Url)
+    time.sleep(5)
+    driver.get(Url)
+    soup =BeautifulSoup(driver.page_source, 'html.parser')
+    primary_item = soup.findAll('div', attrs={'class': 'css-1asz3by'})
+    for i, sub_item in enumerate(primary_item):
+    
+        item_name = sub_item.find(
+            'div', attrs={'class': 'prd_link-product-name'}).text
+        item_price = sub_item.find(
+            'div', attrs={'class': 'prd_link-product-price'}).text
+        item_link = sub_item.find(
+           'a', attrs={'class': 'pcv3__info-content'}).get('href')
+            
+        print(item_name, item_price, item_link)       
+    page += 1
+    newurl = mainUrl + f'&page={page}'
+    print(newurl)
+    web_crawl(newurl,page,primaryUrl)
+
+   
+        
+    
 
 
-# then crawl the website data(not yet done)
-def dfs_web_crawl():
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    link = soup.findAll('div', attrs={'class': 'css-1asz3by'})
-    print(link)
-
-
-dfs_web_crawl()
+web_crawl(primaryUrl,page,primaryUrl)
+print(visited)
